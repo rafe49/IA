4fc9876f182ff781c3a4c658c08541f020efa788
Rafael murta <107264808+rafe49@users.noreply.github.com>
2024-09-13 08:39:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ const perguntas = [
         enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
+                texto: " assustador!",
                 afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
             },
             {
-                texto: "Isso é maravilhoso!",
+                texto: " maravilhoso!",
                 afirmacao: "Quis saber como usar IA no seu dia a dia."
             }
         ]
